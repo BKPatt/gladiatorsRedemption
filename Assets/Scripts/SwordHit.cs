8f5d2985e6906cf04c17278409ab0c4d9b5f2db9
Nicholas Miller <146672486+Nicholas18396@users.noreply.github.com>
2023-10-26 20:49:41 -0400

    Scene fixed with AI and Character

--- Assets/Scripts/SwordHit.cs
+++ Assets/Scripts/SwordHit.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 public class SwordHit : MonoBehaviour
 {
     public int damage = 10;  // Damage value to apply to the enemy
+    public PlayerMovement playerMovement;
 
     private void OnTriggerEnter(Collider other)
     {
         // Check if the collided object is an enemy
         if (other.CompareTag("Enemy"))
         {
-            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
-            if (enemyHealth != null)
+			bool inAttackPlayer = playerMovement.inAttackPlayer;
+			Debug.Log(inAttackPlayer);
+			
+            if (inAttackPlayer == true)
             {
-                enemyHealth.TakeDamage(damage);
-                Debug.Log("Hit an enemy!");
+                EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(damage);
+                    Debug.Log("Hit by player!");
+                }
             }
         }
     }
