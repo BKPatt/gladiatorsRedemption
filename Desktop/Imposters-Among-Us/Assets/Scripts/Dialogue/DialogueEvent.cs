818f23f4b1ebf7ff9a8210d06e46109905b12600
Sheryl <sliu865@gatech.edu>
2023-12-02 14:08:20 -0800

    Delete Desktop directory

--- Desktop/Imposters-Among-Us/Assets/Scripts/Dialogue/DialogueEvent.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-public class DialogueEvent : EventArgs
-{
-    public string SceneName { get; set; }
-}
--- Desktop/update/Imposters-Among-Us/Assets/Scripts/Dialogue/DialogueOption.cs
+++
@@ -1,7 +0,0 @@
-﻿[System.Serializable]
-public class DialogueOption
-{
-    public string text;
-    public int nextSceneIndex;
-    public string nextSceneName;
-}
